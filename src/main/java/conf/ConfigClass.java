8a9e1e39e64e2f9ab52bdb2cf682e38ae8fdb687
Delimuhametov Teimur <teimur.delimuhametov@gmail.com>
2020-10-15 03:33:16 +0300

    mixte 1 and 2

--- src/main/java/conf/ConfigClass.java
+++ src/main/java/conf/ConfigClass.java
@@ -1,9 +1,24 @@
 package conf;
 
+import model.Animal;
+import model.Cat;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(value = "model")
 public class ConfigClass {
+
+    @Bean
+    Cat getCat() {
+        return new Cat();
+    }
+
+    @Bean
+    Animal getAnimal() {
+        Animal animal = new Animal();
+        animal.setCat(getCat());
+        return animal;
+    }
 }
--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,18 +1,11 @@
 package model;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Animal {
 
     private Cat cat;
 
-    public Cat getCat() {
-        return cat;
-    }
+    public Cat getCat() { return cat; }
 
-    @Autowired
     public void setCat(Cat cat) {
         this.cat = cat;
     }
